d027de3740595506d4bbaa9b394d4592290423fa
Sonu <chaudharysonu2614@gmail.com>
2023-11-10 09:13:56 +0530

    done

---
+++ src/index.js
@@ -0,0 +1,69 @@
+//require('dotenv').config({path:'./env'})
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//      1st way
+// import mongoose from "mongoose";
+
+// function connectDB(){}
+
+// connectDB()
+
+
+
+//      2nd way
+/*
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+import express from "express"
+
+const app=express()
+
+( async () => {
+    try{
+        mongoose.connect(`${process.env.
+            MONGODB_URI}/${DB_NAME}`)
+
+            app.on("error",(error)=>{
+                console.log("ERROR",error);
+                throw error
+            })
+
+            app.listen(process.env.PORT, () =>{
+                console.log(`App is listening on port ${process.env.PORT}`);
+            })
+    }
+    catch(error){
+        console.error("ERROR : ",error)
+        throw err
+    }
+})()
+*/
